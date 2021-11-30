d6198687e44e8b1f069302f850389fc3464b1451
Aritrya Senapati <85305838+Aritrya07@users.noreply.github.com>
2021-11-30 19:59:09 +0530

    Update p1.py

--- python lab/offline lab/24.11.21/p1.py
+++ python lab/offline lab/24.11.21/p1.py
@@ -1,5 +1,5 @@
 '''
-2. A store charges ₹120 per item if you buy less than 10 items. If you buy between 10 and 99 items, the
+1. A store charges ₹120 per item if you buy less than 10 items. If you buy between 10 and 99 items, the
 cost is ₹100 per item. If you buy 100 or more items, the cost is ₹70 per item. Write a program that asks
 the user how many items they are buying and prints the total cost.
 '''
